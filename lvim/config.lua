fffbe823c2128ed4c6d3446af25faab58d9d02e2
Gabriel <gabrielsclimaco@protonmail.com>
2023-12-22 01:52:03 -0300

    (lvim) change colorscheme

--- lvim/config.lua
+++ lvim/config.lua
@@ -12,7 +12,7 @@ an executable
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
 -- lvim.colorscheme = "pywal"
-lvim.colorscheme = "catppuccin-mocha"
+lvim.colorscheme = "catppuccin-macchiato"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 vim.opt.clipboard = "unnamed"
